d2254d9431935baff2cd60e6fc8caec0269a1ae9
LatteM <marcaidameg@gmail.com>
2019-05-20 08:58:25 +0800

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,7 +16,7 @@ class UsersTableSeeder extends Seeder
             ['id' => 1, 'name' => 'Trams Admin', 'email' => 'admin@trams.com', 'password' => bcrypt('admin123'), 'role_id' => 1],
             ['id' => 2, 'name' => 'Trams North Dock Officer', 'email' => 'northdock@trams.com', 'password' => bcrypt('northdock'), 'role_id' => 2],
             ['id' => 3, 'name' => 'Trams Security Officer', 'email' => 'security@trams.com', 'password' => bcrypt('security123'), 'role_id' => 3],
-            ['id' => 4, 'name' => 'Trams South Dock Officer', 'email' => 'southdock@trams.com', 'password' => bcrypt('southdock'), 'role_id' => 3],
+            ['id' => 4, 'name' => 'Trams South Dock Officer', 'email' => 'southdock@trams.com', 'password' => bcrypt('southdock'), 'role_id' => 4],
         ];
 
         DB::table('users')->insert($users);
